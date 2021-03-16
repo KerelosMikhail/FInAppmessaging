3bc2a48f331039fd09b49e81de6be87b68131bce
Kerelos Mikhail <kerelos.adel@gmail.com>
2021-03-15 20:30:09 -0400

    Adding example

---
+++ app/src/main/java/com/kerelos/finappmessaging/MainActivity.java
@@ -0,0 +1,16 @@
+package com.kerelos.finappmessaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// App ID : eGu9Dx8WYWs    // I/FIAM.Headless: Starting InAppMessaging runtime with Instance ID YOUR_APP_ID
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
